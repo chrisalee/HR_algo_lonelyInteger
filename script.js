cdeee88badc3a13734c11d4e58673148bc40ba46
Christopher L <56335217+chrisalee@users.noreply.github.com>
2021-10-07 08:19:23 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+const lonelyinteger = (a) => {
+    a.sort();
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] === a[i +1]) {
+            i = a.lastIndexOf(a[i]);
+            continue;
+        } else {
+            console.log(a[i]);
+            return a[i];
+        }
+    }
+
+}
